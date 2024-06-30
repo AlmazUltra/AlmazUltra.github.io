5c731236abf6d5c39b4cb4b9e0d9d1abefb7d6c8
AlmazUltra <157521309+AlmazUltra@users.noreply.github.com>
2024-06-30 14:45:14 +0000

    keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,16 @@
+from aiogram.types import WebAppInfo
+from aiogram import types
+
+web_app = WebAppInfo(url='https://almazultra.github.io/')
+
+keyboard = types.ReplyKeyboardMarkup(
+    keyboard=[
+        [types.KeyboardButton(text='Site', web_app=web_app)]
+    ],
+    resize_keyboard=True
+)
+
+cb = CallbackData('btn', 'action')
+key = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton('Pay', callback_data='btn:buy')]]
+)
